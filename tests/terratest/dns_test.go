25941482c94b3a54800e2f0b9085dedcdcd3ad12
Roy Marcelino <rcomarceli@gmail.com>
2023-02-09 16:58:40 -0500

    more changes to try and get custom function to retry with non fatal errors

--- tests/terratest/dns_test.go
+++ tests/terratest/dns_test.go
@@ -73,7 +73,7 @@ func TestDns(t *testing.T) {
 	// response from below will result in 200s, not 301
 	http_helper.HttpGetWithRetryWithCustomValidation(t, httpsUrl, nil, 50, 5*time.Second, validateHtml)
 
-	returnedString, errReturn := retry.DoWithRetryE(t, "HTTP GET to URL THING", 50, 5*time.Second, func() (string, error) {
+	returnedString := retry.DoWithRetry(t, "HTTP GET to URL THING", 50, 5*time.Second, func() (string, error) {
 		// url := "http://example.com"
 		// expectedRedirectUrl := "http://www.example.com"
 
@@ -83,18 +83,18 @@ func TestDns(t *testing.T) {
 		response, err := http.Get(targetUrl)
 		if err != nil {
 			// t.Fatalf("Failed to GET URL %s: %s", targetUrl, err)
-			return "failed to get url", err
+			return "", ThisThingFailed{Url: targetUrl}
 		}
 
 		if response.StatusCode != http.StatusMovedPermanently {
 			// t.Fatalf("Expected HTTP status code %d but got %d", http.StatusMovedPermanently, response.StatusCode)
-			return "expected wrong status code", err
+			return "", ThisThingFailed{Url: targetUrl}
 		}
 
 		redirectedUrl := response.Request.URL.String()
 		if redirectedUrl != expectedRedirectUrl {
 			// t.Fatalf("Expected URL to redirect to %s but got %s", expectedRedirectUrl, redirectedUrl)
-			return "expected url to redirect", err
+			return "", ThisThingFailed{Url: targetUrl}
 		}
 		response.Body.Close()
 
@@ -103,74 +103,16 @@ func TestDns(t *testing.T) {
 	})
 
 	logger.Logf(t, "returnedString is %s", returnedString)
-	if errReturn != nil {
-		t.Fatalf("returned err %s", errReturn)
-	}
 
 	// http_helper.HttpGetWithRetryWithCustomValidation(t, anotherUrl, nil, 50, 5*time.Second, validateRedirect)
 	// http_helper.HttpGetWithRetryWithCustomValidation(t, wwwUrl, nil, 50, 5*time.Second, validateRedirect)
 }
 
-// func validateRedirect(statusCode int, body string) bool {
-// 	if statusCode != 301 {
-// 		return false
-// 	}
-// 	// could validate body here
-// 	return true
-// }
+type ThisThingFailed struct {
+	Url string
+}
 
-// func TestUrlRedirect(t *testing.T, timeout, url, expectedRedirectUrl) {
-// 	// url := "http://example.com"
-// 	// expectedRedirectUrl := "http://www.example.com"
-
-// 	client := http.Client{
-// 		// By default, Go does not impose a timeout, so an HTTP connection attempt can hang for a LONG time.
-// 		Timeout: time.Duration(timeout) * time.Second,
-// 		// Include the previously created transport config
-// 		// Transport: tr,
-// 	}
-
-// 	response, err := http.Get(url)
-// 	if err != nil {
-// 		t.Fatalf("Failed to GET URL %s: %s", url, err)
-// 	}
-// 	defer response.Body.Close()
-
-// 	if response.StatusCode != http.StatusMovedPermanently {
-// 		t.Fatalf("Expected HTTP status code %d but got %d", http.StatusMovedPermanently, response.StatusCode)
-// 	}
-// 	redirectedUrl := response.Request.URL.String()
-// 	if redirectedUrl != expectedRedirectUrl {
-// 		t.Fatalf("Expected URL to redirect to %s but got %s", expectedRedirectUrl, redirectedUrl)
-// 	}
-
-// }
-
-// func HttpGetWithOptionsE(t testing.TestingT, options HttpGetOptions) (int, string, error) {
-// 	logger.Logf(t, "Making an HTTP GET call to URL %s", options.Url)
-
-// 	// Set HTTP client transport config
-// 	tr := http.DefaultTransport.(*http.Transport).Clone()
-// 	tr.TLSClientConfig = options.TlsConfig
-
-// 	client := http.Client{
-// 		// By default, Go does not impose a timeout, so an HTTP connection attempt can hang for a LONG time.
-// 		Timeout: time.Duration(options.Timeout) * time.Second,
-// 		// Include the previously created transport config
-// 		Transport: tr,
-// 	}
-
-// 	resp, err := client.Get(options.Url)
-// 	if err != nil {
-// 		return -1, "", err
-// 	}
-
-// 	defer resp.Body.Close()
-// 	body, err := ioutil.ReadAll(resp.Body)
-
-// 	if err != nil {
-// 		return -1, "", err
-// 	}
-
-// 	return resp.StatusCode, strings.TrimSpace(string(body)), nil
-// }
+func (err ThisThingFailed) Error() string {
+	return fmt.Sprintf("Validation failed for URL %s", err.Url)
+	// return fmt.Sprintf("Validation failed for URL %s. Response status: %d. Response body:\n%s", err.Url, err.Status, err.Body)
+}
