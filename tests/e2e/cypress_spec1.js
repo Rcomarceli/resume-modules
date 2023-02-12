99f03c6b6c6f7a2b9c8ff73ea32866d1267f86fc
Roy Marcelino <rcomarceli@gmail.com>
2023-02-12 15:06:13 -0500

    attempt to add cypress e2e testing via github actions

---
+++ tests/e2e/cypress_spec1.js
@@ -0,0 +1,23 @@
+describe('The Home Page', () => {
+    it('increments the visitor counter', () => {
+      
+        
+        cy.visit('http://rcmarceli-testing.com'); // change URL to match your dev URL
+  
+  
+  
+        cy.get('[data-cy="visitorCount"]').contains(/\d+/).then(($count) =>{
+            const count1 = parseInt($count.text());
+            console.log(count1);
+            cy.window().its('API_URL').then(url => {
+                // cy.request('POST', url);
+                // cy.request('GET', url).its('body').then(text => parseInt(text)).should('be.gt', count1);
+                cy.request('POST', url).its('body').then(text => parseInt(text)).should('be.gt', count1);
+            })
+  
+        });
+  
+  
+    // cy.request('POST', cy.window().its('API_URL'));
+    })
+  })
