6352c1802d36d51e199693a416563f4c711631c5
Roy Marcelino <rcomarceli@gmail.com>
2023-03-15 16:25:46 -0400

    set program from bash to sh

--- frontend/main.tf
+++ frontend/main.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket_website_configuration" "application" {
 # generates env file for our vite build. used to inject the api_url when building in the pipeline
 # outputs a "dest" value that points to our "dist" build folder
 data "external" "application" {
-  program = ["bash", "/generateEnv.sh"]
+  program = ["sh", "${path.module}/src/envGenerate.sh"]
 
   working_dir = "${path.module}/src"
   query = {
