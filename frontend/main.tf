f650da16dc8a2ff0fd9923ce4a9e4137a4d90ff6
Roy Marcelino <rcomarceli@gmail.com>
2023-03-15 17:50:33 -0400

    fmt

--- frontend/main.tf
+++ frontend/main.tf
@@ -64,7 +64,7 @@ resource "aws_s3_bucket_website_configuration" "application" {
 # generates env file for our vite build. used to inject the api_url when building in the pipeline
 # outputs a "dest" value that points to our "dist" build folder
 data "external" "application" {
- program = ["bash", "-c", <<EOT
+  program = ["bash", "-c", <<EOT
   echo "VITE_API_URL=$(jq -r '.API_URL')" > .env.local && (npm ci && npm run build) >&2 && echo "{\"dest\": \"dist\"}"
   EOT
   ]
