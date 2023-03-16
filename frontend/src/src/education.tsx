0f39f5073ce4007666f5ecdba0e64f8debb5d9c8
Roy Marcelino <rcomarceli@gmail.com>
2023-03-16 19:53:15 -0400

    small changes to education text

--- frontend/src/src/education.tsx
+++ frontend/src/src/education.tsx
@@ -2,7 +2,7 @@ export const educationData = [
     {
         "title": "Old Dominion University",
         "subtitle": "Norfolk, VA | Fall 2013 - Fall 2017",
-        "shortDescription": "Bachelor of Science in Applied Mathematics. Studied multiple disciplines of mathematics including Calculus, Algorithms, Linear Algebra, and Partial Derivatives as well as history of mathematics.",
+        "shortDescription": "Bachelor of Science in Applied Mathematics. Studied multiple disciplines of mathematics including Calculus, Algorithms, Linear Algebra, and Partial Derivatives.",
     },
     {
         "title": "Ritsumeikan Asia Pacific University",
