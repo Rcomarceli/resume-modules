dcd07191f0d7f8f0b282ea54e339d6b4e187facb
Roy Marcelino <rcomarceli@gmail.com>
2023-03-17 01:10:33 -0400

    more fixes

--- backend/cloudflare_worker/api_reverseproxy.js
+++ backend/cloudflare_worker/api_reverseproxy.js
@@ -4,7 +4,9 @@ addEventListener('fetch', event => {
   
   async function handleRequest(request) {
     // should forward original request to the api endpoint defined in plain text binding
-    const response = await fetch('http://' + api_endpoint, request);
+
+    // api endpoint already has the https scheme built in
+    const response = await fetch(api_endpoint, request);
   
     return response;
   }
