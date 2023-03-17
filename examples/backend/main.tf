313462431ee8dc7eb6175e4abf16197f01b84f98
Roy Marcelino <rcomarceli@gmail.com>
2023-03-16 22:43:35 -0400

    added cloudflare provider to backend example

--- examples/backend/main.tf
+++ examples/backend/main.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
   }
 
   required_version = ">= 1.2.0"
